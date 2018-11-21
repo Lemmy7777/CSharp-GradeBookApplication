6d5748ae3026b61344b6868c62d409b08067dbfb
Lemmy7777 <stafford@hcmfx.net>
2018-11-21 07:34:32 -0700

    Update for GradeBookType type

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+    }
+}
